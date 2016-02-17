18c9241376db6fd5fe668b32040642b1f97cb8e3
Eduardo <edubaik@gmail.com>
2016-02-17 12:58:17 -0800

    made everything default

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -20,55 +20,7 @@ class Users extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->model("User");
-		$info["users"] = $this->User->get_all_users();
-		$this->load->view('all_users', $info);
-	}
-	public function new_user()
-	{
-		$this->load->view("new_user");
-	}
-	public function create()
-	{
-		$this->load->library('form_validation');
-		$this->form_validation->set_error_delimiters('<p class="errors">', '</p>');
-		$this->form_validation->set_rules('first_name', 'First Name', 'required');
-		$this->form_validation->set_rules('last_name', 'Last Name', 'required');
-		$this->load->model("User");
-
-		if($this->form_validation->run() === FALSE)
-		{
-			$this->session->set_flashdata("errors", validation_errors());
-			redirect("/users/new_user");
-		}
-		else {
-			$this->User->insert_user($this->input->post());
-			redirect("/users");
-		}
-	}
-	public function show($user_id)
-	{
-
-		$this->load->model("User");
-		$info["user"] = $this->User->get_user($user_id);
-		$this->load->view("user_show", $info);
-	}
-	public function edit($user_id)
-	{
-		$this->load->model("User");
-		$info["user"] = $this->User->get_user($user_id);
-		$this->load->view("edit_user", $info);
-	}
-	public function update($user_id)
-	{
-		$this->load->model("User");
-		$this->User->update_user($user_id, $this->input->post());
-		redirect("/users/show/$user_id");
-	}
-	public function updating()
-	{
-		die();
-		echo "hello";
+		$this->load->view('users_index');
 	}
 }
 
