d6ec9691b9eafe38230cda3757e3d032472b775e
Rich Whybrew <richwhybrew@gmail.com>
2016-02-24 13:36:32 -0800

    Put in top and bottom nav bars for the main landing page. Can be used site wide for consistent styling

--- application/views/index.php
+++ application/views/index.php
@@ -3,7 +3,8 @@
 	<head>
 		<meta charset="utf-8"/>
 		<title></title>
-		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="index.css">
 		<meta name="description" content="insert description"/>
 		<script src= 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
 		<script type="text/javascript">
@@ -13,6 +14,27 @@
 		</script>
 	</head>
 	<body>
-	test
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+      		<div class="container">
+	        	<div class="navbar-header">
+			        <a class="navbar-brand navbar-right" href="#">Sign In</a>
+	        	</div>
+      		</div>
+      		<div class="container">
+      			<!-- insert featured item image slider here -->
+      		</div>
+      		<div class="container">
+      			<!-- insert three category divs here -->
+      		</div>
+      		<div class="container">
+      			<nav class="navbar navbar-inverse navbar-fixed-bottom">
+      				<div class="navbar-bottom">
+      					<a id="about" class="navbar-brand navbar-bottom" href="#">About Us</a>
+      					<a class="navbar-brand navbar-bottom" href="#">Contact Us</a>
+      					<p class="navbar-brand navbar-bottom">&copy; 2016 HealthyNinja</p>
+      				</div>
+      			</nav>
+      		</div>
+    	</nav>
 	</body>
 </html>
