d429b78dc05c0d2082614d6cb763788eba70dd43
gkwong <gkwong85@hotmail.com>
2016-02-24 13:17:04 -0800

    Added login_verification, get_user, and add_user functions with queries

--- application/models/User.php
+++ application/models/User.php
@@ -2,15 +2,32 @@
 
 Class User extends CI_Model
 {
-	public function get_user()
+	/*this function will query the database for an email/password match to login the user*/
+	public function login_verification($post) 
 	{
 		$query = "SELECT * FROM users WHERE email = ? AND password = ?";
 		$values = array($post['email'], md5($post['password']));
 		return $this->db->query($query, $values)->row_array();
 	}
-
-	public function add_user()
+	/*this function will query the database for a matching user ID and return first_name, last_name,
+	email, billing_street, billing_city, billing_state, billing_zip, shipping_street, shipping_city,
+	shipping_state, and shipping_zip*/
+	public function get_user($id)
 	{
-		
+		$query = "SELECT * FROM users where id = ?"
+		$values = array($post['first_name'], $post['last_name'], $post['email'], $post['billing_street'], $post['billing_city'], $post['billing_state'],
+			$post['billing_zip'], $post['shipping_street'], $post['shipping_city'], $post['shipping_state'], $post['shipping_zip']);
+		return ($query, $values)->row_array();
+	}
+	/*this function will register a user*/
+	public function add_user($post)
+	{
+		$query = "INSERT INTO users (first_name, last_name, email, password, billing_street, 
+			billing_city, billing_state, billing_zip, shipping_street, shipping_city, shipping_state, 
+			shipping_zip, admin_status, last_login, created_at, updated_at VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,NOW(),NOW(),NOW())";
+		$values = array($post['first_name'], $post['last_name'], $post['email'], $post['password'], $post['billing_state'], $post['billing_city'],
+			$post['billing_state'], $post['billing_zip'], $post['shipping_street'], $post['shipping_city'], $post['shipping_state'],
+			$post['shipping_zip'], 'user');
+		$this->db->query($query, $values);
 	}
 }
