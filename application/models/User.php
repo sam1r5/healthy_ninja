a95cfe3c7a6806aa2b78921ad8625681730e936c
gkwong <gkwong85@hotmail.com>
2016-02-24 13:34:57 -0800

    Missed a semicolon in login_verification function, added a delete_user function and added md5 to password of add_user function.

--- application/models/User.php
+++ application/models/User.php
@@ -14,7 +14,7 @@ Class User extends CI_Model
 	shipping_state, and shipping_zip*/
 	public function get_user($id)
 	{
-		$query = "SELECT * FROM users where id = ?"
+		$query = "SELECT * FROM users where id = ?";
 		$values = array($post['first_name'], $post['last_name'], $post['email'], $post['billing_street'], $post['billing_city'], $post['billing_state'],
 			$post['billing_zip'], $post['shipping_street'], $post['shipping_city'], $post['shipping_state'], $post['shipping_zip']);
 		return ($query, $values)->row_array();
@@ -25,9 +25,16 @@ Class User extends CI_Model
 		$query = "INSERT INTO users (first_name, last_name, email, password, billing_street, 
 			billing_city, billing_state, billing_zip, shipping_street, shipping_city, shipping_state, 
 			shipping_zip, admin_status, last_login, created_at, updated_at VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,NOW(),NOW(),NOW())";
-		$values = array($post['first_name'], $post['last_name'], $post['email'], $post['password'], $post['billing_state'], $post['billing_city'],
+		$values = array($post['first_name'], $post['last_name'], $post['email'], md5($post['password']), $post['billing_state'], $post['billing_city'],
 			$post['billing_state'], $post['billing_zip'], $post['shipping_street'], $post['shipping_city'], $post['shipping_state'],
 			$post['shipping_zip'], 'user');
+		$this->db->query($query, $values);
+	}
+	/*this function will delete the user*/
+	public function delete_user($id)
+	{
+		$query = "DELETE FROM users WHERE id = ?";
+		$values = array($id);
 		$this->db->query($query, $values);
 	}
 }
