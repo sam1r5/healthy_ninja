140233e9e6ed9d53fe69b9991507411234f9cd05
Rich Whybrew <richwhybrew@gmail.com>
2016-02-24 12:31:15 -0800

    Added database model and sql file in the assets/data folder. Also added dummy views in the views folder. Will need to put them in subfolders later after we make controllers.

---
+++ application/views/admin_dash.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<meta name="description" content="insert description"/>
+		<script src= 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
+		<script type="text/javascript">
+			$(document).ready(function(){
+				// jQuery codes here
+			});
+		</script>
+	</head>
+	<body>
+
+	</body>
+</html>
