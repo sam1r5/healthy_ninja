adc275e287ab89534a582795545ae378a97e4d1a
Samir Sachdev <ssachdev13@gmail.com>
2016-02-24 13:05:29 -0800

    Added products as the main controller to routes. Also added an index function to products that lods a view for index.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,6 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 // this is the default controller. You need to change it depending on your project
-$route['default_controller'] = 'users';
+$route['default_controller'] = 'products';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
--- application/views/index.php
+++ application/views/index.php
@@ -13,6 +13,6 @@
 		</script>
 	</head>
 	<body>
-
+	test
 	</body>
 </html>
