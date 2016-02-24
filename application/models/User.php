f239f9ce68e53d6a99bcccb45a3cf7993a411297
Rich Whybrew <richwhybrew@gmail.com>
2016-02-24 13:42:13 -0800

    Added parenthesis in add_user function in User model

--- application/models/User.php
+++ application/models/User.php
@@ -24,7 +24,7 @@ Class User extends CI_Model
 	{
 		$query = "INSERT INTO users (first_name, last_name, email, password, billing_street, 
 			billing_city, billing_state, billing_zip, shipping_street, shipping_city, shipping_state, 
-			shipping_zip, admin_status, last_login, created_at, updated_at VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,NOW(),NOW(),NOW())";
+			shipping_zip, admin_status, last_login, created_at, updated_at) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,NOW(),NOW(),NOW())";
 		$values = array($post['first_name'], $post['last_name'], $post['email'], md5($post['password']), $post['billing_state'], $post['billing_city'],
 			$post['billing_state'], $post['billing_zip'], $post['shipping_street'], $post['shipping_city'], $post['shipping_state'],
 			$post['shipping_zip'], 'user');
