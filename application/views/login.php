774cce5d1e306e1a6c8d978f71e36999b319efdd
Rich Whybrew <richwhybrew@gmail.com>
2016-02-24 14:21:53 -0800

    Added login and registration views.

--- application/views/login.php
+++ application/views/login.php
@@ -3,7 +3,9 @@
 	<head>
 		<meta charset="utf-8"/>
 		<title></title>
-		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="signin.css">
+		<link rel="stylesheet" type="text/css" href="index.css">
 		<meta name="description" content="insert description"/>
 		<script src= 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
 		<script type="text/javascript">
@@ -13,6 +15,31 @@
 		</script>
 	</head>
 	<body>
-
+		<div class="container">
+			<nav class="navbar navbar-inverse navbar-fixed-top">
+	      		<div class="container">
+		        	<div class="navbar-header">
+				        <a class="navbar-brand navbar-right" href="#">Sign In</a>
+		        	</div>
+	      		</div>
+	      	</nav>
+     	</div>
+		<div class="container">
+          	<form class="form-signin" action="/Users/login" method="post">
+            <h2 class="form-signin-heading">Login</h2>
+            <input type="text" name="login_username" class="form-control" placeholder="Email" required autofocus>
+            <input type="password" name="login_password" class="form-control" placeholder="Password" required>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+          </form>
+    	</div>
+    	<div class="container">
+      		<nav class="navbar navbar-inverse navbar-fixed-bottom">
+      			<div class="navbar-bottom">
+      				<a id="about" class="navbar-brand navbar-bottom" href="#">About Us</a>
+      				<a class="navbar-brand navbar-bottom" href="#">Contact Us</a>
+      				<p class="navbar-brand navbar-bottom">&copy; 2016 HealthyNinja</p>
+      			</div>
+      			</nav>
+      	</div>
 	</body>
 </html>
