cbecafb2f8624c37deddcdb3d8bcbcecc8735bab
Rich Whybrew <richwhybrew@gmail.com>
2016-02-25 14:47:07 -0800

    Added functions in controlers/models to pull product info, load product page, add reviews, and pull reviews to display on products/product view.

--- application/controllers/Reviews.php
+++ application/controllers/Reviews.php
@@ -1,10 +1,11 @@
 <?php 
 
 class Reviews extends CI_Controller {
-	function add_review($post)
+	function add_review()
 	{
-		
+		$this->load->model('Review');
+		$this->Review->add_review($this->input->post());
+		redirect('/Reviews/get_reviews') // Need to reload reviews after we add a new one.
 	}
 }
-
  ?>
