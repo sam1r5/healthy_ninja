99b688697afe77b2b3037dd78eda342282eaa87f
Samir Sachdev <ssachdev13@gmail.com>
2016-02-26 17:49:52 -0800

    Added update_quantity, add_product, delete_item to the Carts and Cart model and controller. Made the cart view and added the table with update and delete functionality

--- application/models/Cart.php
+++ application/models/Cart.php
@@ -4,15 +4,31 @@ class Cart extends CI_Model
 	public function get_user_cart()
 	{
 		$id = $this->session->userdata('id');
-		$query = "SELECT users.first_name, users.last_name, products.name, cart_relationships.quantity FROM users LEFT JOIN carts On users.id = carts.user_id
-		LEFT JOIN cart_relationships on cart_relationships.cart_id = carts.id
-		LEFT JOIN products on cart_relationships.product_id = products.id WHERE user.id = $id";	
-		return $this->db->query($query)->result_array();
+		$query = "SELECT users.first_name, users.last_name, products.name, cart_relationships.quantity, products.id FROM users LEFT JOIN carts On users.id = carts.user_id LEFT JOIN cart_relationships on cart_relationships.cart_id = carts.id LEFT JOIN products on cart_relationships.product_id = products.id WHERE users.id = ?";
+		$values = array($id);	
+		return $this->db->query($query, $values)->result_array();
 	}
 
-	public function add_product()
+	public function add_product($post)
 	{
-		
+		$query = "INSERT INTO cart_relationships (quantity, product_id, cart_id, created_at, updated_at) VALUES (?,?,?,?,?)";
+		$values = array($post['quantity'], $post['product_id'], $post['cart_id'], date('Y-m-d, H:i:s'), date('Y-m-d, H:i:s'));
+		$this->db->query($query, $values);
+	}
+
+	public function update_quantity($post)
+	{
+		$query = "UPDATE cart_relationships SET 
+		quantity = ?  WHERE product_id = ? AND cart_id in(SELECT id from carts WHERE user_id = ?)";
+		$values = array($post['quantity'], $post["product_id"], $this->session->userdata('id'));
+		$this->db->query($query, $values);
+	}
+
+	public function delete_item($post)
+	{
+		$query = "DELETE FROM cart_relationships WHERE product_id = ?";
+		$values = array($post['product_id']);
+		$this->db->query($query, $values);
 	}
 
 }
--- application/views/cart.php
+++ application/views/cart.php
@@ -1,14 +1,25 @@
 <!DOCTYPE html>
+
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title></title>
+		<title>Cart</title>
+		<link rel="stylesheet" type="text/css" href="/assets/stylesheets/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="/assets/stylesheets/index.css">
+		<link rel="stylesheet" type="text/css" href="/assets/stylesheets/cart.css">
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<meta name="description" content="insert description"/>
 		<script src= 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
 		<script type="text/javascript">
 			$(document).ready(function(){
 				// jQuery codes here
+				
+				$("form").on("change", function() {
+					$(this).submit();
+				})
+				// $("select").on("change", function() {
+				//     $(".cart").submit();
+				// });
 			});
 		</script>
 	</head>
@@ -27,6 +38,55 @@
 	      		</div>
 	      	</nav>
       	</div>
+
+      	      	<table class="table table-hover" align-"center">
+      		<thead>
+      			<tr>
+      				<td>Product Name</td>
+      				<td>Product Quantity</td>
+      				<td>Update Quantity</td>
+      				<td>Delete</td>
+      			</tr>
+      		</thead>
+      		<tbody>
+
+<?php 
+					foreach($items as $item)
+					{
+ ?>
+	      			<tr>
+ 						<td><?php echo $item['name'] ?></td>
+ 						<td><?php echo $item['quantity'] ?></td>
+ 						<td>
+	 						<form action="/carts/update_quantity" method="post" class="cart">
+	 							<input	type="hidden" name="product_id" value="<?= $item['id'] ?>">			
+ 								<select name="quantity" class='quantity'> 
+ 									<option></option>
+ 									<option value='1'>1</option>
+ 									<option value='2'>2</option>
+ 									<option value='3'>3</option>
+ 									<option value='4'>4</option>
+ 									<option value='5'>5</option>
+ 									<option value='6'>6</option>
+ 									<option value='7'>7</option>
+ 									<option value='8'>8</option>
+ 									<option value='9'>9</option>
+ 									<option value='10'>10</option>
+ 								</select>
+							</form>
+						</td>
+						<td>
+							<form action="/carts/delete_item" method=post >
+								<input	type="hidden" name="product_id" value="<?= $item['id'] ?>">
+								<button type="submit" >Delete</button>
+							</form>
+						</td>
+	      			</tr>
+ <?php 
+ 					}
+  ?>
+      		</tbody>
+      	</table>
 <?php	}?>
 <?php 	if(!$this->session->userdata('id'))
 		{ ?>
@@ -39,6 +99,9 @@
 	      		</div>
 	      	</nav>
       	</div>
+
+
+
 <?php  	} ?>
 		<nav class="navbar navbar-inverse navbar-fixed-bottom">
 			<div class="navbar-bottom">
