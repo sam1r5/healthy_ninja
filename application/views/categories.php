c87db222e9f2b96ae6a7174819c84ed2fbebf83c
gkwong <gkwong85@hotmail.com>
2016-03-01 16:13:21 -0800

    Made minor changes to view page of categories to show price

--- application/views/categories.php
+++ application/views/categories.php
@@ -49,7 +49,7 @@
 		<div class="category_images">
 			<img src="/assets/images/<?php echo $product['name'] ?>.jpg">
 			<div class="view">
-			<h3><?php echo $product['name'] ?></h3>
+			<h3><?php echo $product['name'] . " - $" . $product['price']?></h3>
 			<form action="/Products/load_product_page" method="post">
 				<input type="hidden" name="product_id" value="<?php echo $product['id'] ?>">
 				<input type="Submit" name="view" value="View Product">
