1e807d2a45d74c523080d652c63d91c3c6c35bbd
gkwong <gkwong85@hotmail.com>
2016-03-02 13:43:50 -0800

    Made style changes to categories page

--- application/views/categories.php
+++ application/views/categories.php
@@ -49,10 +49,10 @@
 		<div class="category_images">
 			<img src="/assets/images/<?php echo $product['name'] ?>.jpg">
 			<div class="view">
-			<h3><?php echo $product['name'] . "- $" . $product['price']?></h3>
+			<h3><?php echo $product['name'] . " - $" . $product['price']?></h3>
 			<form action="/Products/load_product_page" method="post">
 				<input type="hidden" name="product_id" value="<?php echo $product['id'] ?>">
-				<input id="product" class="btn btn-lg btn-primary btn-block" type="Submit" name="view" value="View Product Info">
+				<input id="product" class="btn btn-lg btn-primary btn-block" type="Submit" name="view" value="Product Info">
 			</form>
 			</div>
 		</div>
