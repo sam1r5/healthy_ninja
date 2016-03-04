ea784317ff28b77c6063c03efe15afb26b4be9f2
gkwong <gkwong85@hotmail.com>
2016-03-04 08:53:18 -0800

    Fixed contact us view

--- application/views/contact_us.php
+++ application/views/contact_us.php
@@ -8,6 +8,7 @@
 		<link rel="stylesheet" type="text/css" href="/assets/stylesheets/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="/assets/stylesheets/contact_us.css">
 		<link rel="stylesheet" type="text/css" href="/assets/stylesheets/index.css">
+		<link rel="stylesheet" type="text/css" href="/assets/stylesheets/signin.css">
 		<script src= 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
 		<script type="text/javascript">
 			$(document).ready(function(){
@@ -56,16 +57,16 @@
 		</iframe>
 	</div>
 	<div class="container" class="input-form">
-		<h2 class="form-signin-heading" class="center">Email Us</h2>
-		<form method='post' action='/users/contactvalidate' class="form-horizontal" >
+		<h2>Email Us</h2>
+		<form method='post' action='/users/contactvalidate' class="form-signin" >
 		<div class="form-group">
 				<span class='red_text'><?php echo form_error('name');?></span>
-			<label class="form-control" class="col-sm-2 control-label">Name</label>
+			<label>Name</label>
 				<input type='text' name='name' class="form-control" required>
-			<label class="form-control" class="col-sm-2 control-label">Email</label>
+			<label>Email</label>
 				<span class='red_text'><?php echo form_error('information');?></span>
 				<input type='email' name='email' class="form-control" required>
-			<label class="form-control" class="col-sm-2 control-label">Message</label>
+			<label>Message</label>
 				<textarea placeholder="Email us" name='information' class="form-control" required></textarea>
 			
 			<button class="btn btn-lg btn-primary btn-block" type="submit" name='action' value="submit" type="submit">Contact Us</button>
